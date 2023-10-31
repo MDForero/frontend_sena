6b752cc2ea331e67e656cddcf1d66ee4999d5adb
Michael Forero <mforeroroldan@gmail.com>
2023-10-31 11:23:27 -0500

    ya se ve el listado de facturas

--- app/dashboard/facturacion/page.js
+++ app/dashboard/facturacion/page.js
@@ -1,6 +1,30 @@
-export default async function page() {
-    return <div>
-        <h1>Facturación</h1>
-    </div>
-}
+import { Th, Td } from "@/app/components/Table"
+import useInvoices from "@/app/hooks/invoices"
 
+export default async function page() {
+    const data = await useInvoices().getData()
+    return <>
+        <table>
+            <thead>
+                <tr>
+                    <Th>id</Th>
+                    <Th>Nombre</Th>
+                    <Th>Estado</Th>
+                    <Th>Valor</Th>
+                    <Th>Nit</Th>
+                    <Th>Telefono</Th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item) => <tr key={item.id}>
+                    <Td>{item.id}</Td>
+                    <Td>{item.name}</Td>
+                    <Td>{item.status}</Td>
+                    <Td>{item.value}</Td>
+                    <Td>{item.nit}</Td>
+                    <Td>{item.phone}</Td>
+                </tr>)}
+            </tbody>
+        </table>
+    </>
+}
