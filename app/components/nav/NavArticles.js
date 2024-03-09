099acb0cee2e38ec27999795e6423a1517bee923
Michael Forero <mforeroroldan@gmail.com>
2024-03-09 14:17:52 -0500

    se cambio la estructura de carpetas para encontrarlas un poco mas organizadas

---
+++ app/components/nav/NavArticles.js
@@ -0,0 +1,25 @@
+'use client'
+
+import Link from "next/link"
+import { useAuth } from "../../hooks/auth"
+
+const NavArticles = () => {
+    const { user } = useAuth()
+    return (
+        <>  
+        {user && user.role === 'admin' &&
+            <nav className="p-2">
+                <ul className="flex gap-2">
+                    <li className=" border-2 rounded-xl  w-28 ">
+                        <Link href="/dashboard/articulos/agregar" className="flex flex-row-reverse justify-center items-center p-2 gap-2"><svg viewBox="0 0 32 32" className="w-5 h-5" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">  <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" > <g id="Icon-Set-Filled" transform="translate(-362.000000, -1037.000000)" fill="#000000"> <path d="M390,1049 L382,1049 L382,1041 C382,1038.79 380.209,1037 378,1037 C375.791,1037 374,1038.79 374,1041 L374,1049 L366,1049 C363.791,1049 362,1050.79 362,1053 C362,1055.21 363.791,1057 366,1057 L374,1057 L374,1065 C374,1067.21 375.791,1069 378,1069 C380.209,1069 382,1067.21 382,1065 L382,1057 L390,1057 C392.209,1057 394,1055.21 394,1053 C394,1050.79 392.209,1049 390,1049" id="plus"> </path> </g> </g> </g></svg> Agregar</Link>
+                    </li>
+                    <li className=" border-2 rounded-xl w-28 ">
+                        <Link href="/dashboard/articulos/editar" className="flex flex-row-reverse justify-center items-center p-2 gap-2"><svg viewBox="0 0 24 24" className='W-5 h-5' fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg> Editar</Link>
+                    </li>
+                </ul>
+            </nav>}
+        </>
+    )
+}
+
+export default NavArticles
---
+++ app/components/table/TableInvoice.js
@@ -0,0 +1,58 @@
+"use client"
+import React, { useState } from 'react'
+import { Td, Th } from '../Table'
+
+const TableInvoice = ({ invoice }) => {
+    const [search, setSearch] = useState(invoice)
+    let dateMin, dateMax
+
+    const getDate = (date) => new Date(date).getTime()
+    const date = (date) => new Date(date).toDateString()
+
+    
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        console.log()
+        const result = invoice.filter(item => getDate(item.created_at) >= getDate(dateMin.value) && getDate(item.created_at) <= getDate(dateMax.value))
+        setSearch(result)
+    }
+
+    return (<>
+        <form onSubmit={handleSearch}>
+            <input type='date' name='dateMin' ref={node => dateMin = node} />
+            <input type='date' name='dateMax' ref={node => dateMax = node} />
+            <button type='submit'>Buscar</button>
+        </form>
+        <table className="border-2 table-fixed border-separate border-spacing-2">
+            <thead>
+                <tr>
+                    <Th>Fecha</Th>
+                    <Th>id</Th>
+                    <Th>Estado</Th>
+                    <Th>Valor</Th>
+                    <Th>Teléfono</Th>
+                </tr>
+            </thead>
+            <tbody>
+                {search.map((item) => <tr key={item.id}>
+                    <Td>{date(item.created_at)}</Td>
+                    <Td>{item.id}</Td>
+                    <Td>{item.status}</Td>
+                    <Td> {item.value}</Td>
+                    <Td>{item?.address}</Td>
+                </tr>)}
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td className=" text-right">Valor</td>
+                    <td className=" text-center">{search.length !== 0 ?search.map(item => item.value).reduce((acc, item) => acc + item) : NaN}</td>
+                </tr>
+            </tfoot>
+        </table>
+    </>)
+}
+
+export default TableInvoice
